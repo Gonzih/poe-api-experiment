55c69aade0ae2a7f1f602ded84019b033f5ca5b4
Max Gonzih <gonzih@gmail.com>
2018-05-23 21:48:28 -0400

    first commit

---
+++ Makefile
@@ -0,0 +1,11 @@
+proto:
+	protoc -I=. -I=$(GOPATH)/src -I=$(GOPATH)/src/github.com/gogo/protobuf/protobuf --gofast_out=\
+	Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types,\
+	Mgoogle/protobuf/duration.proto=github.com/gogo/protobuf/types,\
+	Mgoogle/protobuf/struct.proto=github.com/gogo/protobuf/types,\
+	Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,\
+	Mgoogle/protobuf/wrappers.proto=github.com/gogo/protobuf/types:. \
+	*.proto
+
+run: proto
+	vgo run *.go
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gogo/protobuf/jsonpb"
+)
+
+// type responseData struct {
+// 	NextChangeID string `json:"next_change_id"`
+// 	Stashes      []struct {
+// 		ID                string  `json:"id"`
+// 		Public            bool    `json:"public"`
+// 		AccountName       *string `json:"accountName"`
+// 		LastCharacterName *string `json:"lastCharacterName"`
+// 		Stash             *string `json:"stash"`
+// 		StashType         string  `json:"stashType"`
+// 		Items             []struct {
+// 			AbyssJewel bool `json:"abyssJewel"`
+// 		} `json:"items"`
+// 	} `json:"stashes"`
+// }
+
+func main() {
+	resp, err := http.Get("http://www.pathofexile.com/api/public-stash-tabs?")
+	if err != nil {
+		panic(err)
+	}
+	var data Response
+
+	defer resp.Body.Close()
+
+	// body, _ := ioutil.ReadAll(resp.Body)
+	// log.Println(string(body))
+	// err = json.Unmarshal(body, &data)
+
+	unmarshaller := jsonpb.Unmarshaler{AllowUnknownFields: true}
+	err = unmarshaller.Unmarshal(resp.Body, &data)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// log.Println(data)
+
+	log.Println("Done!")
+}
