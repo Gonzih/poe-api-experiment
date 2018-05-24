c068992b16b26fd540c0bff7e7532a90cda26c3a
Max Gonzih <gonzih@gmail.com>
2018-05-24 15:22:29 -0400

    Nice

--- .travis-install-protoc.sh
+++ .travis-install-protoc.sh
@@ -5,7 +5,7 @@ set -ex
 if [ ! -f "/usr/bin/protoc" ]; then
   wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip -O protobuf.zip
   uname -a
-  # echo 'a5f0bf7d8534acca7364b2a263910a6d  protobuf.zip' | md5sum --check
+  echo 'a5f0bf7d8534acca7364b2a263910a6d  protobuf.zip' | md5sum --check
   sudo unzip protobuf.zip -d /usr
   sudo chmod +x /usr/bin/protoc
 else
