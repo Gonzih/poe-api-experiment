85a1051b09e02900c36aa3960998264681e57d6d
Max Gonzih <gonzih@gmail.com>
2018-05-26 10:16:09 -0400

    Add more tests

--- input_generation_test.go
+++ input_generation_test.go
@@ -6,6 +6,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestMapFrameType(t *testing.T) {
+	assert.Equal(t, "normal", mapFrameType(0))
+	assert.Equal(t, "magic", mapFrameType(1))
+	assert.Equal(t, "rare", mapFrameType(2))
+	assert.Equal(t, "unique", mapFrameType(3))
+	assert.Equal(t, "gem", mapFrameType(4))
+	assert.Equal(t, "currency", mapFrameType(5))
+	assert.Equal(t, "divination card", mapFrameType(6))
+	assert.Equal(t, "quest item", mapFrameType(7))
+	assert.Equal(t, "prophecy", mapFrameType(8))
+	assert.Equal(t, "relic", mapFrameType(9))
+	assert.Equal(t, "", mapFrameType(999))
+}
+
 func TestPriceParsing(t *testing.T) {
 	testPrices := []string{
 		"~price 3 chaos",
