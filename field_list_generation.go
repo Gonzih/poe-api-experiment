9b6ac81fbbefb1abba8696b5ae6943b1e3be96c9
Max Gonzih <gonzih@gmail.com>
2018-05-26 19:37:17 -0400

    Generate yaml code for fields

--- field_list_generation.go
+++ field_list_generation.go
@@ -1,17 +1,34 @@
 package main
 
-import "log"
+import (
+	fmt "fmt"
+	"log"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type FieldsForExtraction struct {
+	Properties map[string]bool
+}
 
 func generateFields(dbPath string) error {
-	fields := make(map[string]bool, 0)
+	fields := &FieldsForExtraction{
+		Properties: make(map[string]bool, 0),
+	}
+
+	c := 0
 
 	err := walkResponses("data.bin", func(r *Response) error {
 		for _, stash := range r.Stashes {
 			if stash != nil {
 				for _, item := range stash.Items {
 					if item != nil {
-						for _, field := range item.GetAdditionalProperties() {
-							fields[field.GetName()] = true
+						c++
+						if c > 10000 {
+							return fmt.Errorf("beep")
+						}
+						for _, field := range item.GetProperties() {
+							fields.Properties[field.GetName()] = true
 						}
 					}
 				}
@@ -21,11 +38,16 @@ func generateFields(dbPath string) error {
 		return nil
 	})
 
+	// if err != nil {
+	// 	return err
+	// }
+
+	data, err := yaml.Marshal(fields)
+	log.Printf("\n%s", string(data))
+
 	if err != nil {
 		return err
 	}
-
-	log.Println(fields)
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -3,5 +3,6 @@ module github.com/Gonzih/poe-api-experiment
 require (
 	github.com/gogo/protobuf v1.0.0
 	github.com/golang/protobuf v1.1.0
+	gopkg.in/yaml.v2 v2.2.1
 	gorgonia.org/gorgonia v0.8.2
 )
