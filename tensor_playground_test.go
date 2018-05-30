bf12dc1493be56c10a74911d5492bf17ac8be152
Max Gonzih <gonzih@gmail.com>
2018-05-30 10:33:39 -0400

    Sure its not

--- tensor_playground_test.go
+++ tensor_playground_test.go
@@ -10,10 +10,13 @@ import (
 func TestTensor(t *testing.T) {
 	d := tensor.New(tensor.WithBacking([]float32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}), tensor.WithShape(2, 5))
 	fmt.Println(d)
+
 	fmt.Println(d.Sum(0))
 	fmt.Println(d.Sum(1))
 	fmt.Println(d.Size())
 	fmt.Println(d.Dims())
-	fmt.Println(d.Strides())
 
+	fmt.Println(d.T(1, 0))
+	// fmt.Println(d.Transpose())
+	fmt.Println(d)
 }
